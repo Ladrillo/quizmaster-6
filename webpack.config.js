1ed7372ab7e4da146ffa25612fa14cd74f65e11a
Ladrillo <gabriel@cabrejas.net>
2015-10-25 15:23:14 -0600

    a ver

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,8 @@ module.exports = {
     entry: ['./core/client/app.js'],
 
     output: {
-        path: '/build',
-        publicPath: '/build/',
+        path: '/core/client/build',
+        // publicPath: '/build/',
         filename: 'bundle.js'
     },
 
