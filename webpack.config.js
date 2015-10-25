c0fb7382540ea2057d1bcb3e6572ee49189f1a5e
Ladrillo <gabriel@cabrejas.net>
2015-10-25 12:25:49 -0600

    solo join again

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 var path = require('path');
 // var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-var buildPath = __dirname + '/build/';
-var mainPath = __dirname + '/core/client/app.js';
+var buildPath = path.join(__dirname, '/build');
+var mainPath = path.join(__dirname, '/core/client', 'app.js');
 
 module.exports = {
 
