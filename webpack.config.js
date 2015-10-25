a057d138ad2e0c00346507853676a3a9d7201fc0
Ladrillo <gabriel@cabrejas.net>
2015-10-25 13:07:17 -0600

    venga, y mas...

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 
     context: path.resolve(__dirname, 'core/client/'),
 
-    entry: './app',
+    entry: path.resolve('core/client/', 'app.js'),
 
     output: {
         path: path.resolve('/build'),
