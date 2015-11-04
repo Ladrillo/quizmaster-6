9cce7ca42bc395579d264efd1720ea5f91f9505f
Gabriel Cabrejas Poole <gabriel@cabrejas.net>
2015-11-03 21:22:45 -0700

    trying to fix auth when deployed

--- core/server/config/env/production.js
+++ core/server/config/env/production.js
@@ -5,6 +5,6 @@ module.exports = {
     google: {
         clientID: "419787304121-0h5tfaol93fsrahr3ev0pvfs9j072trc.apps.googleusercontent.com",
         clientSecret: "XWSe_3RYz39-nj2gb7dZcar6",
-        callbackURL: "http://localhost:5000/auth/google/oauth2redirect"
+        callbackURL: "https://pure-dusk-8896.herokuapp.com/auth/google/oauth2redirect"
     },
 };
